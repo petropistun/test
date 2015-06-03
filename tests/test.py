8e38e3fa3394b205702bce257399794cc0fe95ad
petro.pistun <petro.pistun@axilera.com>
2015-06-03 12:15:32 +0300

    test

---
+++ tests/test.py
@@ -0,0 +1,9 @@
+import os, sys
+import logging, logging.handlers
+import time
+from pprint import pprint
+
+
+file = open('../README.md', 'r')
+
+print file.read()
---
+++ tests/test2.py
@@ -0,0 +1,11 @@
+import os, sys
+import logging, logging.handlers
+import time
+from pprint import pprint
+
+
+file = open('../README.md', 'r')
+print "=========222222========="
+print file.read()
+print file.read()
+print file.read()
