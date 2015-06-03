6a550cb6c118f48ee8fc120a9f962ff925a791b6
petro.pistun <petro.pistun@axilera.com>
2015-06-03 13:05:07 +0300

    test

--- tests/test.py
+++ tests/test.py
@@ -5,15 +5,18 @@ import time
 from pprint import pprint
 
 
-file = open('README.md', 'r')
 
-print file.read()
-i  = 0
+i = 0
 while 1:
 	print ".", i
+	file = open('out', 'a')
+	file.write(" i = %d\n"%i)
+	file.close()
 	time.sleep(1)
 	i += 1
 	if i > 60:
 		break
+
+
 
 print "====end="
--- tests/testf.py
+++ tests/testf.py
@@ -1,6 +1,7 @@
 import os, sys
 import logging, logging.handlers
 import time
+import subprocess
 from pprint import pprint
 
 import pexpect
@@ -11,8 +12,29 @@ def test_1():
 
 
 
+print "===begin==="
 child = pexpect.spawn('python ./tests/test.py')
-time.sleep(30)
+
+
+
+import subprocess
+#process = subprocess.Popen(['ps', 'aux'],  shell=True, stdout=subprocess.PIPE)
+#out, err = process.communicate()
+subprocess.call("ps ax | grep python", shell=True)
+
+#print("Processs=====", out)
+#print("Processs err=====", err)
+
+time.sleep(10)
+
+file = open('out', 'r')
+
+print "out begin>>>>>"
+print file.read()
+print "<<<<out end"
+
+
+print "===end==="
 
 #test_1()
 
